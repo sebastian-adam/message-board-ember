b3fba3c8f96f1c4ae2cd94905aff8732a238a72e
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-08-18 20:59:59 -0700

    initialize question and answer models

--- tests/unit/routes/board-test.js
+++ tests/unit/routes/board-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:question', 'Unit | Route | question', {
+moduleFor('route:board', 'Unit | Route | board', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
