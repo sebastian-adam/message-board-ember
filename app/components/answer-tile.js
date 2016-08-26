65b2031c8ee57d589c96060f527a87716a6af724
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-08-26 01:27:53 -0700

    add computed property to sort answers by most likes

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   avgLiking: Ember.computed('answer.likes', 'answer.dislikes', function() {
-    return this.get('answer.likes') / this.get('answer.dislikes');
+    var score = this.get('answer.likes') / this.get('answer.dislikes');
+    if (score && isFinite(score)) {
+      return score;
+    } else {
+      return 0;
+    }
   }),
+  actions: {
+    like(answer, question) {
+      this.sendAction('like', answer, question);
+    },
+    dislike(answer, question) {
+      this.sendAction('dislike', answer, question);
+    }
+  }
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['likes:desc'],
+  sortedAnswers: Ember.computed.sort('answers', 'sortBy'),
   actions: {
     destroyQuestion(question) {
       if (confirm('Are you sure you want to delete this question?')) {
