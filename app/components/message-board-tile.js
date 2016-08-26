f797f13601d93770c289bab232a59bf2d21ab17c
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-08-26 13:26:26 -0700

    bind sticky yellow icon to favorited questions using computed property 'checkForFavorite'

--- app/components/message-board-tile.js
+++ app/components/message-board-tile.js
@@ -2,9 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service(),
+  checkForFavorite: Ember.computed('favoritesList', 'question', function() {
+    if ((this.get('favoritesList')).favorites.includes(this.get('question'))) {
+      return true;
+    }
+  }),
   actions: {
     addToFavorites(question) {
       this.get('favoritesList').saveToFavorites(question);
+      this.set('checkForFavorite', true);
     }
   }
 });
