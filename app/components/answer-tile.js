7518ddaa7e939e5984a665c6a70475545744e70e
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-08-26 02:20:42 -0700

    implement the greater-than helper in hiding poorly scored answers; thumbs down icon red for low scoring answers

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  answerIsHidden: true,
   avgLiking: Ember.computed('answer.likes', 'answer.dislikes', function() {
     var score = this.get('answer.likes') / this.get('answer.dislikes');
     if (score && isFinite(score)) {
       return score;
+    } else if (score) {
+      return 1;
     } else {
       return 0;
     }
@@ -15,6 +18,9 @@ export default Ember.Component.extend({
     },
     dislike(answer, question) {
       this.sendAction('dislike', answer, question);
+    },
+    showAnswer(){
+      this.set('answerIsHidden', false);
     }
   }
 });
---
+++ app/helpers/greater-than.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+const greaterThanOne = (params) => params[0] >= .5;
+
+export default Ember.Helper.helper(greaterThanOne);
