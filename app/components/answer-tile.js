a4d57713971ae46cc3b859d3a517bb6e24e97ae3
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-21 19:35:59 -0700

    update computed property in answer tile component to account for exceptions

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   answerIsHidden: true,
   avgLiking: Ember.computed('answer.likes', 'answer.dislikes', function() {
+    var likes = this.get('answer.likes');
+    var dislikes = this.get('answer.dislikes')
     var score = this.get('answer.likes') / this.get('answer.dislikes');
-    if (score && isFinite(score)) {
+    if ((score && isFinite(score)) || (likes === 0 && dislikes !== 0)) {
       return score;
-    } else if (score) {
+    } else if (score || (likes === 0 && dislikes === 0)) {
       return 1;
     } else {
       return 0;
